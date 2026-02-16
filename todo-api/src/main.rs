b149550261865c12c9cd89afae189080ac31d400
kkoouuya0730 <kouya.0730@icloud.com>
2026-02-16 22:53:31 +0900

    hello world

--- todo-api/src/main.rs
+++ todo-api/src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{App, HttpResponse, HttpServer, Responder, get};
+
+#[get("/")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Hello")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    println!("Server running at http://127.0.0.1:8080");
+
+    HttpServer::new(|| App::new().service(hello))
+        .bind(("127.0.0.1", 8080))?
+        .run()
+        .await
 }
