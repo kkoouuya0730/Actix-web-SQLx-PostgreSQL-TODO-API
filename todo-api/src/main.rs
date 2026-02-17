64a6de7b81480cefd9856d4b97de719b58642e9a
kkoouuya0730 <kouya.0730@icloud.com>
2026-02-17 20:56:30 +0900

    Connection Pool作成

--- todo-api/src/main.rs
+++ todo-api/src/main.rs
@@ -1,4 +1,20 @@
-use actix_web::{App, HttpResponse, HttpServer, Responder, get};
+use actix_web::{App, HttpResponse, HttpServer, Responder, get, web};
+use dotenvy::dotenv;
+use sqlx::PgPool;
+use std::env;
+
+struct AppState {
+    pool: PgPool,
+}
+
+async fn health_check(data: web::Data<AppState>) -> impl Responder {
+    let row: (i32,) = sqlx::query_as("SELECT 1")
+        .fetch_one(&data.pool)
+        .await
+        .unwrap();
+
+    HttpResponse::Ok().body(format!("DB OK: {}", row.0))
+}
 
 #[get("/")]
 async fn hello() -> impl Responder {
@@ -9,8 +25,20 @@ async fn hello() -> impl Responder {
 async fn main() -> std::io::Result<()> {
     println!("Server running at http://127.0.0.1:8080");
 
-    HttpServer::new(|| App::new().service(hello))
-        .bind(("127.0.0.1", 8080))?
-        .run()
+    dotenv().ok();
+
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    let pool = PgPool::connect(&database_url)
         .await
+        .expect("Failed to create pool");
+    println!("Connected to DB");
+
+    HttpServer::new(move || {
+        App::new()
+            .app_data(web::Data::new(AppState { pool: pool.clone() }))
+            .route("/health", web::get().to(health_check))
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
